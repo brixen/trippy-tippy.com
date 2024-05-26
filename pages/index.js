d6088d09477ee05a6e9f58c90f31625cdb10c218
Brian Shirai <brixen@gmail.com>
2024-05-26 10:01:29 -0700

    Add template Nextjs app.

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Trippy Tippy</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="">
+          Trippy Tippy
+        </h1>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  );
+}
