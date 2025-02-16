ab8f40637123fab2bfe9505f00bda10c990f830c
Brian Shirai <brixen@gmail.com>
2025-02-16 10:48:55 -0800

    Fix config.

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  distDir: 'out',
+  basePath: '',
+  assetPrefix: '/',
+  trailingSlash: true,
+};
+
+export default nextConfig;
