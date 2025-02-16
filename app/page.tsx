223421ba2da4d06af479633a8dc0c9a5856a6566
Brian Shirai <brixen@gmail.com>
2025-02-15 21:14:21 -0800

    Add hero image.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Image src="/trippy-tippy.jpg" alt="Trippy Tippy"
+        width={3024} height={4032}
+        className="w-full h-auto object-cover"
+        layout="responsive"
+        priority />
     </main>
   );
 }
